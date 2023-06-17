8b1b896538fc2cb10205e028cea7f12c3dc9f5e6
Ludovic Dumas <ludoloops@pm.me>
2023-06-16 22:08:34 -0400

    cpanel config file

---
+++ app.js
@@ -0,0 +1,12 @@
+if (typeof (PhusionPassenger) !== 'undefined') {
+  PhusionPassenger.configure({ autoInstall: false })
+}
+
+const strapi = require('@strapi/strapi')
+const distDir = './dist'
+if (process.env.NODE_ENV == "development") {
+  strapi({ "autoReload": { "enabled": true }, distDir }).start()
+}
+else {
+  strapi({ distDir }).start()
+}
