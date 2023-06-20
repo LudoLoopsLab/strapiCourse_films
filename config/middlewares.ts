6e7ab47d07b0dac9ee0d090afb7ab82502bec959
Ludovic Dumas <ludoloops@pm.me>
2023-06-20 01:33:16 -0400

    edit cors

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,7 +1,6 @@
 export default [
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
@@ -9,4 +8,12 @@ export default [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
-];
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      headers: '*',
+      origin: ['*']
+    }
+  },
+]
